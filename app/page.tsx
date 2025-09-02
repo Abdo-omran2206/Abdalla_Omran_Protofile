7f1a7c1579b47f65d94e51741b9201c2a84fe719
akira-omran <kirabot77@gamil.com>
2025-09-02 11:06:57 +0300

    new: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
     <>
-
+      <header className="flex justify-center items-center p-4">
+        <Navbar />
+      </header>
     </>
   );
+}
+
+
+
+export function Navbar(){
+  return(
+    <nav className="backdrop-blur-md bg-white/10 border border-white/20 shadow-lg  p-6 w-auto flex sticky px-5 py-3 rounded-4xl">
+      <ul className="flex gap-15 text-white font-bold">
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Projects">Projects</a></li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+    </nav>
+  )
 }
