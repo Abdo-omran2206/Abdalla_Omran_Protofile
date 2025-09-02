84536a2f974f4c3eda0e4bdffa1dca74dda020f3
akira-omran <kirabot77@gamil.com>
2025-09-02 11:13:20 +0300

    edit: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
 
 export function Navbar(){
   return(
-    <nav className="backdrop-blur-md bg-white/10 border border-white/20 shadow-lg  p-6 w-auto flex sticky px-5 py-3 rounded-4xl">
-      <ul className="flex gap-15 text-white font-bold">
+    <nav className="backdrop-blur-md bg-white/10 border border-white/20 shadow-lg w-auto flex fixed px-15 py-3 rounded-4xl mt-15">
+      <ul className="flex gap-20 text-white font-bold text-xl">
         <li><a href="#Home">Home</a></li>
         <li><a href="#About">About</a></li>
-        <li><a href="#Projects">Projects</a></li>
+        <li><a href="#Portfolio">Portfolio</a></li>
         <li><a href="#Contact">Contact</a></li>
       </ul>
     </nav>
